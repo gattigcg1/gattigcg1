8c56c822e56862807465e0f481bd580cd08bcd89
Giancarlo Gatti <149108218+gattigcg1@users.noreply.github.com>
2025-05-28 23:59:21 +0800

    Update WN10-AU-000505.ps1

--- STIGS/WN10-AU-000505.ps1
+++ STIGS/WN10-AU-000505.ps1
@@ -1,3 +1,30 @@
+<#
+.SYNOPSIS
+    This PowerShell script ensures the Security event log size must be configured to 1024000 KB or greater.
+
+.NOTES
+    Author          : Giancarlo Gatti
+    LinkedIn        : https://www.linkedin.com/in/giancarlo-gatti-80948b2a5
+    GitHub          : github.com/gattigcg1
+    Date Created    : 2024-28-05
+    Last Modified   : 2024-28-05
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN10-AU-000500
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\__remediation_template(STIG-ID-WN10-AU-000505).ps1 
+#>
+
 # Define the registry path
 $regPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\EventLog\Security"
 
