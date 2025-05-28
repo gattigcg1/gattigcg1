7a09a93415be91f04052ab084173ff2c3f5c5503
Giancarlo Gatti <149108218+gattigcg1@users.noreply.github.com>
2025-05-29 01:05:50 +0800

    Create WN10-CC-000238.ps1

---
+++ STIGS/WN10-CC-000238.ps1
@@ -0,0 +1,37 @@
+<#
+.SYNOPSIS
+    This PowerShell script ensures that the system event log size must be configured to 32768 KB or greater. 
+
+.NOTES
+    Author          : Giancarlo Gatti
+    LinkedIn        : https://www.linkedin.com/in/giancarlo-gatti-80948b2a5
+    GitHub          : github.com/gattigcg1
+    Date Created    : 2025-29-05
+    Last Modified   : 2025-29-05
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN10-CC-000238
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\__remediation_template(STIG-ID-WN10-CC-000238).ps1 
+#>
+
+# Define the registry path
+$regPath = "HKLM:\SOFTWARE\Policies\Microsoft\MicrosoftEdge\Internet Settings"
+
+# Ensure the registry key exists
+if (-not (Test-Path $regPath)) {
+    New-Item -Path $regPath -Force
+}
+
+# Set the PreventCertErrorOverrides DWORD value to 0x1 (1 decimal)
+New-ItemProperty -Path $regPath -Name "PreventCertErrorOverrides" -PropertyType DWord -Value 0x1 -Force
